9807b9196ea05c8759a86a568fa20e88db111f21
Antonio Alan <tonio.alan07@gmail.com>
2023-05-25 16:04:33 -0300

    limpando projeto e add pastas

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles/generic/global.css'
+import './styles/settings/colors.css'
 import App from './App.jsx'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
