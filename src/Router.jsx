d3e0d574cc3fda5b7d106296d5ca0c45f26b606b
Antonio Alan <tonio.alan07@gmail.com>
2023-05-28 19:09:26 -0300

    criação Container component

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,15 +3,18 @@ import { Home } from "./pages/Home";
 import { Favorites } from "./pages/Favorites";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { Container } from "./components/Container";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </Container>
       <Footer />
     </BrowserRouter>
   )
