c5a0ec78da13aa233a0842b21f2fbb0d0a93bb2e
Antonio Alan <tonio.alan07@gmail.com>
2023-05-28 18:38:42 -0300

    captura dinâmica de dados do Card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import { Header } from "../../components/Header";
 import { Banner } from "../../components/Banner";
 import { Title } from "../../components/Title";
 import { Card } from "../../components/Card";
+import videos from '../../json/db.json';
 
 export const Home = () => {
   return (
@@ -13,7 +14,11 @@ export const Home = () => {
       <Title>
         <h1>Um lugar para guardar seu jogos!</h1>
       </Title>
-      <Card id='1' title="Alan" cover="https://avatars.githubusercontent.com/u/70543343?v=4" />
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />
+        })}
+      </section>
       <Footer />
     </>
   )
