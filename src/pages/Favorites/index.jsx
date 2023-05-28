53d035bc7f8ec208af84ae5a040a37334a6e28f0
Antonio Alan <tonio.alan07@gmail.com>
2023-05-28 19:18:04 -0300

    add components na Favorites page

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,8 +1,18 @@
 import styles from "./styles.module.css";
+import { Banner } from "../../components/Banner";
+import { Title } from "../../components/Title";
+import { Card } from "../../components/Card";
 
 export const Favorites = () => {
   return (
     <>
+        <Banner img="fav" />
+        <Title>
+            <h1>Meus Favoritos</h1>
+        </Title>
+        <section className={styles.container}>
+            <Card id="1" title="Persona 3" cover="https://cdn.akamai.steamstatic.com/steam/apps/1809700/header.jpg?t=1679398544" />
+        </section>
     </>
   )
 }
