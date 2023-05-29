9b6c0e9d72df535edc46944a8aa63838b61ff6a0
Antonio Alan <tonio.alan07@gmail.com>
2023-05-29 11:25:18 -0300

    mudança de estado no icone de favoritar

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,26 @@
 import styles from "./styles.module.css";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { useFavoriteContext } from "../../contexts/Favorites";
 
 export const Card = ({ id, title, cover }) => {
+  const { favorite, addFavorite } = useFavoriteContext();
+  const isFavorite = favorite.some((fav) => fav.id === id);
+  
   return (
     <div className={styles.container}>
-        <img src={cover} alt={title} className={styles.cover} />
-        <h2>{title}</h2>
-        <AiOutlineHeart className={styles.fav} />
+      <img src={cover} alt={title} className={styles.cover} />
+      <h2>{title}</h2>
+      <div className={styles.fav} alt="Favorita filme">
+          {isFavorite
+            ? <AiFillHeart onClick={() => {
+              addFavorite({ id, title, cover })
+            }}/>
+            : <AiOutlineHeart onClick={() => {
+              addFavorite({ id, title, cover })
+            }} />
+          }
+      </div>
+
     </div>
   )
 }
